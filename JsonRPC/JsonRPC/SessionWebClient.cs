040bf8172375f3edd021b19c1beb6dcc21746ae0
Peter Schuster <schuster.pe@gmail.com>
2015-03-11 00:54:09 +0100

    Implementation for cookie based sessions.

---
+++ JsonRPC/JsonRPC/SessionWebClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+
+namespace JsonRPC
+{
+    internal class SessionWebClient : WebClient
+    {
+        private readonly CookieContainer cookieContainer;
+
+        public SessionWebClient()
+        {
+            this.cookieContainer = new CookieContainer();
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            WebResponse response = base.GetWebResponse(request);
+
+            this.GetCookies(response);
+
+            return response;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request, IAsyncResult result)
+        {
+            WebResponse response = base.GetWebResponse(request, result);
+
+            this.GetCookies(response);
+
+            return response;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+
+            var httpRequest = request as HttpWebRequest;
+
+            if (httpRequest != null)
+            {
+                httpRequest.CookieContainer = this.cookieContainer;
+            }
+
+            return request;
+        }
+
+        private void GetCookies(WebResponse response)
+        {
+            var httpResponse = response as HttpWebResponse;
+
+            if (httpResponse == null)
+                return;
+
+            this.cookieContainer.Add(httpResponse.Cookies);
+        }
+    }
+}
